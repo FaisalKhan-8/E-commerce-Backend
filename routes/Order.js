f6658324587349f4fc2dd254f5a43ebb68368aa1
Faisal Khan <md.fk@somaiya.edu>
2023-09-05 14:39:20 +0530

    Created MVC / integrated all api --Final MVC

--- routes/Order.js
+++ routes/Order.js
@@ -4,14 +4,16 @@ import {
   fetchOrdersByUser,
   deleteOrder,
   updateOrder,
+  fetchAllOrders,
 } from '../controller/Order';
 
 const router = express.Router();
 //  /orders is already added in base path
 router
   .post('/', createOrder)
-  .get('/', fetchOrdersByUser)
+  .get('/user/:userId', fetchOrdersByUser)
   .delete('/:id', deleteOrder)
-  .patch('/:id', updateOrder);
+  .patch('/:id', updateOrder)
+  .get('/', fetchAllOrders);
 
 export default router;
