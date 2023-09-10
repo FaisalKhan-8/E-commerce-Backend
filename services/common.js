17d1682cffd1291730f85e39cc2b7fe07cc29fd9
Faisal Khan <md.fk@somaiya.edu>
2023-09-10 12:37:37 +0530

    final

--- services/common.js
+++ services/common.js
@@ -14,7 +14,6 @@ export function cookieExtractor(req) {
     token = req.cookies['jwt'];
   }
   //TODO : this is temporary token for testing without cookie
-  token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZjlhNzIyNmI5YzE4YzkxZjIyMGEwMCIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY5NDMyNzc1NX0.LmU8BPlsR-7QKkLKfAyCt1qASv079QyipPghd3Vj3y0';
+  // token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZjlhNzIyNmI5YzE4YzkxZjIyMGEwMCIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY5NDMyNzc1NX0.LmU8BPlsR-7QKkLKfAyCt1qASv079QyipPghd3Vj3y0';
   return token;
 }
