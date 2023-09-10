779dcca97264051a4d55d6522cd1552ab99381cc
Faisal Khan <md.fk@somaiya.edu>
2023-09-10 13:20:51 +0530

    changes for deploye

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ import ordersRouter from './routes/Order';
 import User from './model/User';
 import { isAuth, sanitizeUser, cookieExtractor } from './services/common';
 import path from 'path';
+import Order from './model/Order';
 
 // Webhook is here.
 
@@ -34,7 +35,7 @@ const endpointSecret = process.env.ENDPOINT_SECRET;
 server.post(
   '/webhook',
   express.raw({ type: 'application/json' }),
-  (request, response) => {
+  async (request, response) => {
     const sig = request.headers['stripe-signature'];
 
     let event;
@@ -50,8 +51,11 @@ server.post(
     switch (event.type) {
       case 'payment_intent.succeeded':
         const paymentIntentSucceeded = event.data.object;
-        console.log({ paymentIntentSucceeded });
-        // Then define and call a function to handle the event payment_intent.succeeded
+        const order = await Order.findById(
+          paymentIntentSucceeded.metadata.orderId
+        );
+        order.paymentStatus = 'received';
+        await order.save();
         break;
       // ... handle other event types
       default:
@@ -98,6 +102,10 @@ server.use('/users', isAuth(), userRouter);
 server.use('/auth', authRouter);
 server.use('/cart', isAuth(), cartRouter);
 server.use('/orders', isAuth(), ordersRouter);
+// this line we add to make react router work in case of other routes doesnt match
+server.get('*', (req, res) =>
+  res.sendFile(path.resolve('build', 'index.html'))
+);
 
 // Passport Strategies
 passport.use(
@@ -177,7 +185,7 @@ passport.deserializeUser(function (user, cb) {
 const stripe = require('stripe')(process.env.STRIPE_SERVER_KEY);
 
 server.post('/create-payment-intent', async (req, res) => {
-  const { totalAmount } = req.body;
+  const { totalAmount, orderId } = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
@@ -185,6 +193,9 @@ server.post('/create-payment-intent', async (req, res) => {
     currency: 'inr',
     automatic_payment_methods: {
       enabled: true,
+    },
+    metadata: {
+      orderId,
     },
   });
 
